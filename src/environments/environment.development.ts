7788defef9b494b8c91349f67851cf6deec98c6b
driars <driars0329@gmail.com>
2023-10-02 14:15:56 +0200

    Add environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc',
+  apiKey: process.env['NG_APP_API_KEY'],
+};
