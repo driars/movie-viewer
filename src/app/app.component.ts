8ee16404d9272b6203d9ce04b870c38e4d3438e1
driars <driars0329@gmail.com>
2023-10-02 13:32:25 +0200

    Add components list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'movie-viewer';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('header', { static: true }) headerElement?: ElementRef;
+
+  ngAfterViewInit(): void {
+    const headerHeight = this.headerElement?.nativeElement.clientHeight;
+    document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThemeSwitcherComponent
+    ThemeSwitcherComponent,
+    MovieDetailComponent,
+    MovieListComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule
